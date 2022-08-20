7d3c7071569bd84b8bfbe6fe0c216356ea9dc0d3
Anthony-S58 <tonysim01@hotmail.com>
2022-08-20 13:31:34 +0200

    Library install and import

---
+++ main.py
@@ -0,0 +1,10 @@
+#web scrapping jeux videos
+
+#installer les librairies
+#pip install requests
+#pip install beautifulsoup4
+#pip install pandas
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
