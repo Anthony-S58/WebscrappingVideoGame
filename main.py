a9d863e7e36e9309c8568b1cba8149bf7cee4780
Anthony-S58 <tonysim01@hotmail.com>
2022-08-25 22:03:46 +0200

    GamePageInformations

--- main.py
+++ main.py
@@ -26,24 +26,17 @@ driver = launchdriver()
 #recuperer le bouton pour passer les cookies
 cookies_button = driver.find_element(By.CLASS_NAME, "jad_cmp_paywall_button-cookies")
 cookies_button.click()
-
 #récupérer le bouton de recherche pour activer la barre de recherche
 searchbar_button = driver.find_element(By.CLASS_NAME, "header__navLinkSearch")
 searchbar_button.click()
-
-
 #récupérer la barre de recherche et insérer un texte
 search_bar = driver.find_element(By.ID, "search")
 search_bar.send_keys(game)
-
 #récupérer le bouton pour valider la recherche et effectuer la recherche
 validation_button = driver.find_element(By.CLASS_NAME, "headerSearch__formBtn")
 validation_button.click()
-
 #récupérer la catégorie jeux
-game_categorie = driver.find_element(By.CLASS_NAME, "lien-jv")
-game_categorie.click()
-
-#récupérer tous les noms des jeux
-all_titles = driver.find_element(By.CSS_SELECTOR, "section")
-
+# game_categorie = driver.find_element(By.CLASS_NAME, "lien-jv")
+# game_categorie.click()
+game_link = driver.find_element(By.CLASS_NAME, "card__link")
+game_link.click()
